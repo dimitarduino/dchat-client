6a20699d51245f320601227c77a23b34450fd689
dimitar <dimitarduino@gmail.com>
2021-01-05 00:11:06 +0100

    Priprema za prod: Netlify try: 2

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: process.env.SERVERLINK ? process.env.SERVERLINK : "http://localhost:5000/"
+    baseURL: process.env.REACT_APP_SERVERLINK ? process.env.REACT_APP_SERVERLINK : "http://localhost:5000/"
 })
 
 export default instance
